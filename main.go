345e036c0b7abc075a6718b8350793a15f6c2e0e
wanjoc <85894801+wanjoc@users.noreply.github.com>
2022-09-07 11:29:53 -0500

    Change the Message text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Good Morning World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
